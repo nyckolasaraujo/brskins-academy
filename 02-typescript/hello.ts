4f14f23d423a26f2cada525b36883088c8eee593
nyckolasaraujo <nyckolasaraujo.93@outlook.com>
2026-09-09 18:09:01 -0300

    etapa-02 projeto typescript e hello.ts

---
+++ 02-typescript/hello.ts
@@ -0,0 +1,14 @@
+const nome: string = "BRSKINS";
+const ano: number = 2026;
+
+console.log("Olá, " + nome + "! Estamos no ano de " + ano + ".");
+
+for (let i = 1; i <= 3; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " é par");
+    }
+    else {
+        console.log(i + " é impar");
+
+    }
+}
